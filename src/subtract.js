0bd2996bd297f20432f27d195d78868350a6fa2a
Wassim_Itani <wassim.j.itani@gmail.com>
2023-12-05 13:48:26 +0200

    Create sub fn

---
+++ src/subtract.js
@@ -0,0 +1,10 @@
+/**
+ * Subtracts two numbers
+ * @param {*} num1 - The first number
+ * @param {*} num2 - The second number
+ * @returns The difference between the first and second number
+ */
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
