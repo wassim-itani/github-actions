1fa3a744386f6904dff886da4f3a3b1b9d508197
Wassim_Itani <wassim.j.itani@gmail.com>
2023-12-05 14:08:12 +0200

    Add docs of add fn

---
+++ src/add.js
@@ -0,0 +1,10 @@
+/**
+ * Adds two numbers
+ * @param {*} num1 - The first number
+ * @param {*} num2 - The second number
+ * @returns The sum of first and second number
+ */
+
+function add(num1, num2) {
+  return num1 + num2;
+}
