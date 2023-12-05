8a6dbcba66b431031125074164070e1398e042d3
Wassim_Itani <wassim.j.itani@gmail.com>
2023-12-05 14:32:05 +0200

    Anything

---
+++ src/divide.js
@@ -0,0 +1,10 @@
+/**
+ * Divides two numbers
+ * @param {number} num1 - The first number
+ * @param {number} num2 - The second number
+ * @returns The  division between the first and second number
+ */
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
